152310ccf3e76de46963d3364c77d542d5ad6b8e
Abdul Sami <AbdulSami.Usuf@gmail.com>
2024-01-01 15:34:02 +0500

    Learn Variables

---
+++ Variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String Companyname = "Xtrasoft Footwear";
+  print(Companyname);
+  print("how many floors were there ?");
+  int floors = 4;
+  print(floors);
+  String Verification = "Is Xtrasoft was a verified company ?";
+  print(Verification);
+  bool Verified = true;
+  print(Verified);
+}
