6364cde406f3bcfd36976ada1fbaa1361cf924bb
Abdul Sami <AbdulSami.Usuf@gmail.com>
2024-01-03 14:33:22 +0500

    Learn List

---
+++ list.dart
@@ -0,0 +1,22 @@
+void main() {
+  String Company = "Xtrasoft Footwear";
+  print(Company);
+  print("Names of company brand");
+  List<String> Brands = [
+    "Nike",
+    "Nautica",
+    "Puma",
+    "Space",
+  ];
+  print(Brands);
+  print("Available sizes");
+  List<int> Sizes = [
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+  ];
+  print(Sizes);
+}
