f916604fb63f4f8e6b775257b0ffa65b10a82dcf
Abdul Sami <AbdulSami.Usuf@gmail.com>
2024-01-06 14:27:58 +0500

    Learn Map

---
+++ map.dart
@@ -0,0 +1,26 @@
+void main() {
+  Map<String, String> Department = {
+    "graphic designer": "2nd department\n",
+    "pattern designer": "1st department\n"
+  };
+  print(Department);
+
+  List<Map<String, String>> about = [
+    {
+      "Chorangi": "Near the bridge",
+      "Street no 2": "Karachi\n",
+    },
+    {
+      "Plot number 342": "4th floor building\n",
+    },
+    {
+      "HR department": "Basement",
+      "Designer Department": "1st floor\n",
+    },
+    {
+      "Machine made department": "3rd floor",
+      "Hand made Department": "4th floor\n"
+    }
+  ];
+  print(about);
+}
